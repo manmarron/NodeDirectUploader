ef0965335d53adb9240c71bf8ce652590e4ff74b
Nick <nickmarron@hotmail.co.uk>
2020-09-30 15:58:00 +0100

    .

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
-/*
- Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-/*
- * Import required packages.
- * Packages should be installed with "npm install".
- */
 const express = require('express');
 const aws = require('aws-sdk');
 
-/*
- * Set-up and run the Express app.
- */
 const app = express();
 app.set('views', './views');
 app.use(express.static('./public'));
 app.engine('html', require('ejs').renderFile);
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
 
-/*
- * Configure the AWS region of the target bucket.
- * Remember to change this to the relevant region.
- */
-aws.config.region = 'eu-west-1';
-
-/*
- * Load the S3 information from the environment variables.
- */
-const S3_BUCKET = process.env.S3_BUCKET;
+aws.config.region = 'eu-west-2';
+const S3_BUCKET = 'cranebucket'
 
 /*
  * Respond to GET requests to /account.
@@ -71,11 +47,4 @@ app.get('/sign-s3', (req, res) => {
   });
 });
 
-/*
- * Respond to POST requests to /submit_form.
- * This function needs to be completed to handle the information in
- * a way that suits your application.
- */
-app.post('/save-details', (req, res) => {
-  // TODO: Read POSTed form data and do something useful
-});
+
